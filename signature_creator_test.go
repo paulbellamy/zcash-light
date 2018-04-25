bc1e08ae3e332309bc77638e12b44ed56afff986
Paul Bellamy <paul.a.bellamy@gmail.com>
2018-04-25 23:17:43 +0100

    Update import

--- signature_creator_test.go
+++ signature_creator_test.go
@@ -9,11 +9,11 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/OpenBazaar/zcash-light/testdata"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/txscript"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/conformal/btcec"
+	"github.com/paulbellamy/zcash-light/testdata"
 )
 
 var (
